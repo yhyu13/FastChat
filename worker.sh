e6214251bd9d09f51ad18d53b962197dafd9999b
yhyu13 <yohan680919@gmail.com>
2023-04-30 12:21:51 +0800

    Improve script

--- worker.sh
+++ worker.sh
@@ -1,1 +1,6 @@
-python3 -m fastchat.serve.model_worker --model-name 'vicuna-13b-v1.1' --model-path /home/hangyu5/Documents/Hugging-Face/vicuna-13B-1.1-HF/ --num-gpus 2
+MODEL_NAME='vicuna-13b-v1.1'
+MODEL_PATH=/home/hangyu5/Documents/Hugging-Face/vicuna-13B-1.1-HF/
+DEVICE=cuda
+GPUS='0,1'
+NUM_GPUS=2
+python3 -m fastchat.serve.model_worker --device $DEVICE --model-name $MODEL_NAME --model-path $MODEL_PATH --gpus $GPUS --num-gpus $NUM_GPUS
